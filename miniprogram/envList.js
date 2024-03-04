ba91d6c1a1b02c17eca9dda33e07faa06b511a69
zeroSignal <616201092@qq.com>
2024-03-04 21:51:23 +0800

    [Init] Basic files.

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-3g49a9hwda1ff8d7","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-3g49a9hwda1ff8d7 --n quickstartFunctions --r --project "C:\Users\Lenovo\WeChatProjects\miniprogram-2" --report_first --report
